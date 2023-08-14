5fd65d9f41b33e8c3cb39dddf37a178ef25f94fd
kazimalaleem <137309572+kazimalaleem@users.noreply.github.com>
2023-08-14 12:40:29 +0000

    strings.js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,35 @@
+const name = "kaizm"
+const repocount = 50
+
+console.log(name + repoCount + "value");
+
+console.logo(`hello my name is ${name}and my repo countis${repoCount}`);
+
+const gameName = new String('kazim-hc')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.charAt(t));
+
+
+const newString = gameName.substring(0,4)
+console.log(newString)
+
+const anotherstring = gameName.slice(-8, 4)
+console.log(anotherstring);
+
+const newStringOne = "  kazim   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(url.split('-'));
+

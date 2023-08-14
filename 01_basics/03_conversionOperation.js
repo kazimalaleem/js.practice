b66e2cc518f6154520f0517a66ac419371cc3709
kazimalaleem <137309572+kazimalaleem@users.noreply.github.com>
2023-08-14 06:23:49 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "kazim"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abcd" => NaN
+// true => 1; false => 0
+
+
+let isloggedIn = "kazim"
+
+let booleanIsloggedIn = Boolean(isloggedIn)
+
+// console.log(booleanIsloggedIn);
+// 1 => true; 0 => flase
+// "" => flase
+// "kazim" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);

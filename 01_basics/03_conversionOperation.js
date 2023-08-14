0c3425897209882da74ca7e74101e695491a2004
kazimalaleem <137309572+kazimalaleem@users.noreply.github.com>
2023-08-14 10:01:09 +0000

    datatypes summary.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -11,7 +11,6 @@ let valueInNumber = Number(score)
 // "33abcd" => NaN
 // true => 1; false => 0
 
-
 let isloggedIn = "kazim"
 
 let booleanIsloggedIn = Boolean(isloggedIn)
@@ -21,9 +20,54 @@ let booleanIsloggedIn = Boolean(isloggedIn)
 // "" => flase
 // "kazim" => true
 
-
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *************************************** Operations *************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "kazim"
+
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let nam1, nam2, nam3
+nam1 = nam2 = nam3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// let x = 3;
+// const y = x++;
+// console.log(`x:${x}, y:${y}`);
+// // Expected output: "x:4, y:3"
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,32 @@
+// Primitive
+
+//7 types : string, Numeber, null, undefined, symblo, BigInt, 
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp =null
+let userEmail;
+
+const id =Symblo('123')
+const anotherId = Symbol(;123)
+
+console.log(id === anotherId);
+
+// const bigNumber = 23272636281706843237n
+
+// Refrence (Non primitive)
+
+//  Array, Objects, Functions, 
+
+const heros = ["shakiman", "naagraj", "doga"]
+let myObj = {
+    name:"kazim",
+    age: 19,
+}
+
+function myFunction = function () {
+    console.log("hello kazim");
+}
+console.log(typeof bigNumber);
